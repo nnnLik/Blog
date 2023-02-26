cdbea871e52cffca1b6982388d000319a6febc11
nnnLik <makhmudov_ra@vk.com>
2023-02-26 17:23:03 +0300

    [ADD] init

---
+++ src/main/java/com/javablog/Blog/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.javablog.Blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "Main Page");
+		return "home";
+	}
+}
